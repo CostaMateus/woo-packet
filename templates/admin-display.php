c822ad9cc3dcbddf0a134b9702879197d0c71b83
CostaMateus <costa.mack95@gmail.com>
2023-02-10 20:10:55 -0300

    add: infos adicionais

--- templates/admin-display.php
+++ templates/admin-display.php
@@ -25,11 +25,22 @@
 			if ( $method && $method->id === Woo_Packet_Shipping::ID )
 				$instance = $method->instance_id;
 
-        $url      = esc_url( admin_url( "admin.php?page=wc-settings&tab=shipping&instance_id={$instance}" ) );
-        $text     = __( "Configurações método de entrega", Woo_Packet_Shipping::ID );
+		$base     = "admin.php?page=wc-settings&tab=shipping";
+
+		$url      = ( $instance ) ? "&instance_id={$instance}" : "&zone_id=1";
+        $url      = esc_url( admin_url( $base . $url ) );
+        $text     = __( "Configurações", Woo_Packet_Shipping::ID );
         $link     = "<a href='{$url}' >{$text}</a>";
 
+		if ( !$instance )
+			echo "<p>Necessário criar 'Método de entrega' Woo Packet.</p>";
+
         echo "<p>{$link}</p>";
+		echo "<h4 style='margin-bottom:0;' >ATENÇÃO!!!</h4>";
+		echo "<p style='margin:0;' >Todos os produtos precisam estar cadastrados com:</p>";
+		echo "<p style='margin:0;' > -<b>peso</b> (kg);</p>";
+		echo "<p style='margin:0;' > -<b>medidas</b> (AxLxC);</p>";
+		echo "<p style='margin:0;' > -<b>código NCM</b>;</p>";
     ?>
 
 	<hr>
